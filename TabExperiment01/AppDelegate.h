16c8079953f20bf09c9b059348475d4214801cdb
Ken Murphy <kenmrph@gmail.com>
2016-06-07 14:01:45 -0700

    Initial Commit

---
+++ TabExperiment01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/7/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabExperiment01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/7/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
