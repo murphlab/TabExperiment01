237ee163aa1f0ea88bcba8274b2299004bb9785f
Ken Murphy <kenmrph@gmail.com>
2016-06-08 15:51:58 -0700

    custom class for table vc

---
+++ TabExperiment01/FavoritesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesTableViewController.h
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/8/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesTableViewController : UITableViewController
+
+@end
