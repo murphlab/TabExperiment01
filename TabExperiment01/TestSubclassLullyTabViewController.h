9364ed6eb261333fa16b819bb91f617a06008d57
Ken Murphy <kenmrph@gmail.com>
2016-06-07 17:48:09 -0700

    test tab subclass

---
+++ TabExperiment01/TestSubclassLullyTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestSubclassLullyTabViewController.h
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/7/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import "LullyTabViewController.h"
+
+@interface TestSubclassLullyTabViewController : LullyTabViewController
+
+@end
---
+++ TabExperiment01/TestSubclassLullyTabViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestSubclassLullyTabViewController.m
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/7/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import "TestSubclassLullyTabViewController.h"
+
+@interface TestSubclassLullyTabViewController ()
+
+@end
+
+@implementation TestSubclassLullyTabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
