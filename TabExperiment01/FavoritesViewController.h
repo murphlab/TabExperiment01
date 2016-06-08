bd31eb00036f00dfe7cc2f8618665aaacc4c8efd
Ken Murphy <kenmrph@gmail.com>
2016-06-07 17:35:34 -0700

    Add child vcs

---
+++ TabExperiment01/FavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.h
+//  TabExperiment01
+//
+//  Created by Ken Murphy on 6/7/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesViewController : UIViewController
+
+@end
